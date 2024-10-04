e44aede07a10b6eebff4dfa11d0a29cc71c1d5f2
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2024-10-04 00:59:03 -0300

    Fix ExceptionResponseDTO.java

--- src/main/java/com/example/planner/infra/ExceptionResponseDTO.java
+++ src/main/java/com/example/planner/infra/ExceptionResponseDTO.java
@@ -9,7 +9,7 @@ import lombok.Data;
 
 @Data
 @Builder
-public class ExceptionResponse { 
+public class ExceptionResponseDTO { 
     private String title;
     private HttpStatus status;
     private String details;
